ba3b15d6300bb6285c62256be5e13112b0457482
rmlance <reidlance.rml@gmail.com>
2020-06-05 15:03:40 -0400

    Added start form basics

--- app/javascript/react/components/StartContainer.js
+++ app/javascript/react/components/StartContainer.js
@@ -1,13 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import StartForm from './StartForm'
 
 const StartContainer = props => {
+
   return (
-    <div className="grid-container">
-    <h3>Questions:</h3>
-    <li>Baseline data</li>
-    <li>Generate initial data from baseline form submission</li>
-    <li>Check box for "first time users"</li>
-    <li>Form submission adds on extra baseline datapoint as format: baseline, complete: true, to render first point on "home"</li>
+    <div>
+      <StartForm />
     </div>
   )
 }
---
+++ app/javascript/react/components/StartForm.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const StartForm = props => {
+  return (
+    <div>
+      <form>
+        <label>
+          Bench Press:
+          <input
+            name="bench"
+            id="bench"
+          />
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default StartForm
